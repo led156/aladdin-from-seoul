aaca1a2a80c26a27e4c61504c926156e3acf140e
배석진 <baesj1044@gmail.com>
2024-11-20 12:26:09 +0900

    ✨apt_seq 아파트 id를 통한 거래내역 조회 기능 구현
    Former-commit-id: 6c9eee5795173bf2358194959251288fba938d1d

--- BackEnd/springboot-project/src/main/java/com/aladin/house/service/HouseService.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/service/HouseService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bccc364ff862681178f89b3471c2a5ba211fd0d761a3e25c926b6bb20db15834
-size 572
+oid sha256:644b20b1228ceb5b9a4681a659a7731d14e1145b577c7cb6ecb877356e69a655
+size 760
