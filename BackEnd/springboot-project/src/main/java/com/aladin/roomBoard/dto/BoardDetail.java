e15b289955281014539956e86a2e47f4fe31c952
배석진 <baesj1044@gmail.com>
2024-11-21 13:52:28 +0900

    ✨방 게시물+ 사진 링크 상세 조회
    Former-commit-id: 2c78421dc7653e573e6a6ce3999c5c0b8a84b349

--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/dto/BoardDetail.java
+++
@@ -1,5 +0,0 @@
-package com.aladin.roomBoard.dto;
-
-public class BoardDetail {
-
-}
--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/mapper/BoardMapper.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/mapper/BoardMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a88aa4c4e04fb6fb32b3aa855658ad4b44e3026ae93c93b72df16929992ebb7e
-size 554
+oid sha256:2080d82311280dee0d50566fb4584de4367a072e4c8825f4261c76d339e2be44
+size 771
