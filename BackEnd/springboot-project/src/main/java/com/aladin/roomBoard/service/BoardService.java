c1d275bdf0e133c10a8fef437b9b65cbe6cb7ca0
배석진 <bsj1044@naver.com>
2024-11-21 22:43:40 +0900

    ✨방 게시글 삭제 기능 추가
    Former-commit-id: c8c9660879ef83890ffbc2f1e44b5d0cd058e7ef

--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/BoardService.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80ee6d04f62e52f4b8fe250bbb644bb4b78a26671ba8fdb7d3a7419a3f0f4c37
-size 532
+oid sha256:865a78da690d7a529746b693208e09aaf53d7080c8000fedaeb3b63cf4eb63d7
+size 566
