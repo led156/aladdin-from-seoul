e1e462c47596d203b776e8b314db92acd5486fa9
배석진 <bsj1044@naver.com>
2024-11-22 00:21:59 +0900

    ✨방 게시글 북마크 추가, 삭제, 페이징 조회 기능 추가
    Former-commit-id: ce04fce4816e45276372ac85edf417fdc75210a7

---
+++ BackEnd/springboot-project/src/main/java/com/aladin/bookmark/service/BookmarkServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:79f6c977556012b998a56271376faaaa5852fcd6f621c91e5863d3d630ac2daa
+size 1473
--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/BoardServiceImpl.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/BoardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:818740a67abde1c34e1f73f84c50f8d54ebf1fa14a0674f7f8e05ce420b505a1
-size 4225
+oid sha256:1c03ae7bfe8e2199c3ef962c8d832835c68a214ad85282b2de490cd2993fcb98
+size 4226
