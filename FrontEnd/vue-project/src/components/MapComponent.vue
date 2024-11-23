9c037ddda9d30fa49fcb806222addedead01b374
led156 <led156@naver.com>
2024-11-23 15:59:10 +0900

    ✨ 맵 시/구 폴리곤 추가
    - 확대축소 시 일정 레벨에서 폴리곤 생성
    - 폴리곤 클릭시 해당 폴리곤 중앙으로 이동
    
    
    Former-commit-id: 26bd3a3b4564274fac8fbcaf249d8795ff9953b5

--- FrontEnd/vue-project/src/components/MapComponent.vue
+++ FrontEnd/vue-project/src/components/MapComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec2b9867780e923a3464092a859b5dfb7889e9fee0b3381c1154c99d59b6ae76
-size 10732
+oid sha256:af166cd14a9edb5590993d27acac9b6a2f0b0fb77db869a394fc74c816bcf05c
+size 11126
