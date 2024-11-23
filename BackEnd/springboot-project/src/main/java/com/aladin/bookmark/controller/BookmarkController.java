bd7137ff8f6b74e6bfde58984e99801b5300d343
배석진 <bsj1044@naver.com>
2024-11-24 00:18:54 +0900

    ✨ 방 게시글 북마크 1개 조회 기능 추가
    Former-commit-id: 3bbe4fe4ae6360535b6d35d9b094971ee074478a

--- BackEnd/springboot-project/src/main/java/com/aladin/bookmark/controller/BookmarkController.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/bookmark/controller/BookmarkController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8d441d267b799950720675d1e51f559b7b1ddd148f16fbc4033f0c7243b11d7
-size 3811
+oid sha256:429910b8b3c8b489614dad68dcd49d4a2d19556ce04e7610a5ae111f94c775bb
+size 4182
--- BackEnd/springboot-project/src/main/java/com/aladin/bookmark/service/BookmarkService.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/bookmark/service/BookmarkService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6138a778d336b5df7763d9bc6542ec76b018fdc1ade18567096a0255fde22639
-size 752
+oid sha256:83373df3fb7864980d20059f6859bc58bb8edfe8807de7fc4aa3493b8eb3989e
+size 828
