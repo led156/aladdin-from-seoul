d5a8aa61ec7a78dfca82247dfcd8de43ab63ff6d
배석진 <bsj1044@naver.com>
2024-11-23 22:13:49 +0900

    ♻️  아파트 매물 북마크 1개 정보 쿼리 파라미터로 제공
    Former-commit-id: 61e0cfe83143121f1344314dec70bc2b0060cf57

--- BackEnd/springboot-project/src/main/java/com/aladin/bookmark/dto/DealBookmarkOneRequestDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b04e188f9c57da593ce17294a2793b17a98962ec1a11911407ff3c4466631e5a
-size 210
--- BackEnd/springboot-project/src/main/java/com/aladin/bookmark/mapper/BookmarkMapper.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/bookmark/mapper/BookmarkMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1776199f48155c7cd3b09a8e4b1293444e0a6352de1d6243bb711264c93a25cb
-size 1186
+oid sha256:b32607e73ff8f403cfdaf91e72cb50dddd6ea8eb4fe6963bd46ba26529406810
+size 1126
