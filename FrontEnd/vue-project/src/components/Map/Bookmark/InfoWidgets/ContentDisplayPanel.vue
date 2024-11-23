68b16e87545b50e0ddc301f6846ddeb3dd4dbb93
led156 <led156@naver.com>
2024-11-24 03:58:32 +0900

    ✨ 북마크 상세조회 컴포넌트 생성
    - 사이드바2에 대한 내용을 컴포넌트 뼈대로 구성
    
    
    Former-commit-id: 8640d6f1aab4fa1357732fce87820817f8253174

---
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ContentDisplayPanel.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d48ec8ee8a0acf1b5eaccde4cf8d9098b6ad85ddb99caa4f1df87ee4f8c4632
+size 4704
---
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ShareContent.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1e6b3efc3cf8b8d891d29575d6736cfa4a152d03e9526fe072e34d029b393f6
+size 8200
