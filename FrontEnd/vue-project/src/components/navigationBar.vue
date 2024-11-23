386f9ca62185575ee69040c7f733e3e1b80ac750
led156 <led156@naver.com>
2024-11-23 17:09:08 +0900

    💄 네비게이션바 스크롤 효과 추가
    - 스크롤을 밑으로 내릴 땐 보이지 않음
    
    
    Former-commit-id: f3569a07f225ffad2e7df1f5ac5db0d58a1d6aef

--- FrontEnd/vue-project/src/components/navigationBar.vue
+++ FrontEnd/vue-project/src/components/navigationBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59a97e91ee2da406540f817621f168acde96f7588a7fc684b607c61a209fa327
-size 2990
+oid sha256:12d5977f1408e7d392deea0dac92ed528909b510808f738135561ec1f6b6b60b
+size 3999
