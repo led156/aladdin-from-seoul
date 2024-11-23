100c5a7bd21e2118a9f5de4d5a220886c0e70fab
배석진 <bsj1044@naver.com>
2024-11-23 16:39:09 +0900

    ♻️  방 게시물 수정 시 한번 업로드한 사진 수정 불가능
    Former-commit-id: dc73d0639c43b0bd8cfcd376f218cfab982ad3b5

--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/RoomBoardService.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/RoomBoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5049dc3c0bbf8c209df4620aa3cb9d59f26f3276f1efa7eed0add4f5d95dee1
-size 602
+oid sha256:5b7cb139c6f1e7d5670a012b309485ed0434ec8cd34bf1f0c503e926e4816451
+size 644
--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/RoomBoardServiceImpl.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/RoomBoardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03cfb42095ec91df10af4f3b55d3b847d70995afc9d6c832b468f2bd5dbc07e0
-size 4386
+oid sha256:12c0772cc95097a25104b3e54cc441a071755c73894a0ffd2c394f3611614c5e
+size 4666
