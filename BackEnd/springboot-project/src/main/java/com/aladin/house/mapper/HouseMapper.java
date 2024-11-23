aa9b15ef5e681f2fd2a09359eac4a9e1d9aaac51
배석진 <bsj1044@naver.com>
2024-11-23 12:25:50 +0900

    ♻️ 아파트 거래내역 'no' 기반 커서 페이징
    Former-commit-id: 1be8c707c1f70adc273f8dbf86045af3f9353538

--- BackEnd/springboot-project/src/main/java/com/aladin/house/mapper/HouseMapper.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/mapper/HouseMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e578b3752f5f3206812128239437e3803afd333daa6ae62bb8c587c442934293
-size 738
+oid sha256:c4e353fd4535a8369ef58ee4c7bc14022a430efbafcb9f33a7393a88178e16a7
+size 774
--- BackEnd/springboot-project/src/main/java/com/aladin/house/service/HouseServiceImpl.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/service/HouseServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54755eef2a0c6800589b07c5e62508a8f8af368d28be6585e9bf1fc9377544d5
-size 1064
+oid sha256:5eb48ff1a9f85256d4442cb3f633ff952c469497fceb64d631a5098740ebd331
+size 1125
