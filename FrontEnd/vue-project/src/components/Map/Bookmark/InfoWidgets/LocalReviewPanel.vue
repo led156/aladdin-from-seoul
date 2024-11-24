0ae5a82a07bce34ade96df7fea60586e04e24deb
led156 <led156@naver.com>
2024-11-24 18:35:26 +0900

    ✨ 지역정보 추가
    Former-commit-id: c07edd5d541875653d4461e8d8fc8a9fcfe5dfb1

--- FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/LocalReviewPanel.vue
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/LocalReviewPanel.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:177a1ed33c3da14610c4b7b2b8a2501d3df39d49833d78eb912c24c48306d9fc
-size 165
+oid sha256:0ec609cf134191e13c578b865a679a9490db8fbe4d3b2f372099facf5eb3ae05
+size 10702
