6a51f48ad52bf5481ccbe89f746f10f1693e8c4f
led156 <led156@naver.com>
2024-11-24 19:33:10 +0900

    💄 아키네이터 뷰
    Former-commit-id: 91522018bdd9c831c1ea1f6a75f6bf3989e81af1

---
+++ FrontEnd/vue-project/src/components/Akinator/RestartButton.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38a5b94256cd7bdeacace79186913295a33f8eaccb00163fbe93d281f531b158
+size 178
---
+++ FrontEnd/vue-project/src/components/Akinator/ResultBox.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d24694539ae3b22177b5e9a45ee5ed88adfa954f18cdc2f0b2806390064f2ff0
+size 302
