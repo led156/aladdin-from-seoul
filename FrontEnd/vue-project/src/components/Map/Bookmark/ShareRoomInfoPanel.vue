69590a1b0f57159f0d0af91ec07a4802ed9aa008
led156 <led156@naver.com>
2024-11-24 12:22:49 +0900

    ✨ 가까운 교통정보 추가
    - 북마크된 글을 클릭하면 가까운 버스정류장, 지하철역을 확인가능
    
    
    Former-commit-id: 67f5f8f6f472a2529324f984f69a184a3057e15e

--- FrontEnd/vue-project/src/components/Map/Bookmark/ShareRoomInfoPanel.vue
+++ FrontEnd/vue-project/src/components/Map/Bookmark/ShareRoomInfoPanel.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6118db376e504968fef6f242da7b90fef7f1119912d7742b424ba8c6a4d45fd0
-size 6886
+oid sha256:38e279d6f93d2c949b6dbb529f7a424c5d4c65ab3ab3d49343e6865197eb6525
+size 7031
