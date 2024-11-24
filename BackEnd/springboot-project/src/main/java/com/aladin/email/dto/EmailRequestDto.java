59bf4cb31ecee16dfccb081c72b620efe6601a1e
배석진 <bsj1044@naver.com>
2024-11-25 00:45:03 +0900

    🐛 회원가입-이메일 인증 시, 아이디 요구하지 않아도 되도록 수정
    Former-commit-id: ec1b8e9ead30a54d46a554a8213615212eca554c

--- BackEnd/springboot-project/src/main/java/com/aladin/email/dto/EmailRequestDto.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/email/dto/EmailRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:004a9857a29381cc9962d4b2594dc0f365013accff8fe003ba6adfd37c751ba0
-size 396
+oid sha256:c404e708ffb425317012e51ee6dc182877d5c0e722ffad1b15aca7ef7cacd67c
+size 415
