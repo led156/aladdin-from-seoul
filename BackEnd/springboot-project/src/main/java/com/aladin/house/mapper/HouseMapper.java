be4ca5ef67d0eb7dfa48b0afd536332be6cc8112
배석진 <bsj1044@naver.com>
2024-11-24 22:46:55 +0900

    ✨ 아파트 이름 기반 housecards 페이징 조회 기능 추가
    Former-commit-id: 23fb194d241c9f98822f1a38ac0ddc7b731bde7c

--- BackEnd/springboot-project/src/main/java/com/aladin/house/mapper/HouseMapper.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/mapper/HouseMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61a22a92ef10a069e74ac9abde2b4f587cce51298d84b7b7cd8393b14de3d284
-size 1126
+oid sha256:2a11113d3ff536a9c42e3fc7b65a241265929e3c2bb2748cad8199cb65437395
+size 1288
