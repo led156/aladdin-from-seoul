09e64fa08bc7d7998a3db13a0ffda5f47658654c
배석진 <bsj1044@naver.com>
2024-11-24 17:05:48 +0900

    ♻️ 아파트 거래내역 조회 데이터에 동코드 추가 제공
    Former-commit-id: e58f886dd6cfbc784f87abcc303d98fda759ada1

--- BackEnd/springboot-project/src/main/java/com/aladin/bookmark/dto/DealBookmarkResponseDto.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/bookmark/dto/DealBookmarkResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7ed0d812f5b47272352fe55e94b1d3d5db5582416dae6d85eddc521f7317ac5
-size 726
+oid sha256:c8f28c3e94d67ec06f87ec651e15251b59e3d25b684e26e2993e7d81704d8a57
+size 752
