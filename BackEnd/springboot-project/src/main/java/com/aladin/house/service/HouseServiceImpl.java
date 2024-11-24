58002667a8812b33297c493964112ffb6354c338
배석진 <bsj1044@naver.com>
2024-11-24 23:36:48 +0900

    ♻️ TRI 검색 추천 키워드에 서울 지역 아파트 이름 추가
    Former-commit-id: cac573e1d3bb2a990124edd445947b74d0fd2668

--- BackEnd/springboot-project/src/main/java/com/aladin/house/service/HouseServiceImpl.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/service/HouseServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e49bf22e28d22b84453285f2162770a7be6dccfc1e1458efb709b33e0c2bd8
-size 3151
+oid sha256:bbdb180b299440c3fa0042118e45f0f0dc03c73c3fd49d86756abcdf8c61e48d
+size 3605
