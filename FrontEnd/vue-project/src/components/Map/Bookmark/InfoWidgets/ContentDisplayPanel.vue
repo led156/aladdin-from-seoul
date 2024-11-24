603de259a7d9a5e795fff18b5d2827d493b8128b
led156 <led156@naver.com>
2024-11-24 12:22:49 +0900

    ✨ 가까운 교통정보 추가
    - 북마크된 글을 클릭하면 가까운 버스정류장, 지하철역을 확인가능
    
    
    Former-commit-id: c5f9d4b24b74c8a484a8d3af5ad85cc185f8cac6

--- FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ContentDisplayPanel.vue
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ContentDisplayPanel.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d48ec8ee8a0acf1b5eaccde4cf8d9098b6ad85ddb99caa4f1df87ee4f8c4632
-size 4704
+oid sha256:a944c7088ad069ff9ea7e56612fb09aa3fc47d3562a0be5b74d8d155b106f723
+size 4719
---
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/NearbyStopsPanel.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e229084239e4da47588272b5d0a8bba115deee38de4ae54af20de6fa4440703
+size 7761
