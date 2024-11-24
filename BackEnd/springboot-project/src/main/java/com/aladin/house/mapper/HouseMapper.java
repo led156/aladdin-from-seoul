f61f386ad78fe30a576a727bd32393cd057d6b18
배석진 <bsj1044@naver.com>
2024-11-24 23:36:48 +0900

    ♻️ TRI 검색 추천 키워드에 서울 지역 아파트 이름 추가
    Former-commit-id: af04d9236bcbb442e4501a2d44fec7e31acb3ac3

--- BackEnd/springboot-project/src/main/java/com/aladin/house/mapper/HouseMapper.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/mapper/HouseMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a11113d3ff536a9c42e3fc7b65a241265929e3c2bb2748cad8199cb65437395
-size 1288
+oid sha256:5dd4fe222bf5ed82c7fad87a60c517741a5305de58161e0a85b6981212ec10fe
+size 1442
