d45ef584f4a7b2cd48bb29481528842202363a78
led156 <led156@naver.com>
2024-11-24 12:40:36 +0900

    🐛 관심목록에서 다른글 클릭시 상세조회
    - 이전 글이 계속 남아있던 버그를 고침
    
    
    Former-commit-id: 72768e8c5da5bdd680c1fd9685238ffb757a7f71

--- FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ShareContent.vue
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ShareContent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e6b3efc3cf8b8d891d29575d6736cfa4a152d03e9526fe072e34d029b393f6
-size 8200
+oid sha256:1f5500069e66191a7876df378f6d6a4a93500b9b934d91f9a270fba6be8909c4
+size 8818
