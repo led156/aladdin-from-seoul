75844249f5733d53aea10a9c74e4e0dd2ba6df29
배석진 <bsj1044@naver.com>
2024-11-26 00:41:48 +0900

    ✨ 방 게시물 조회수 Top 3 조회 기능 추가
    Former-commit-id: 373fe133914907ca8e371c76bb898890370a9a34

--- BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/RoomBoardServiceImpl.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/roomBoard/service/RoomBoardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51fff7bece2f734960192804e0121eeeb2470098282b4dbcdda4486a16ae268b
-size 5209
+oid sha256:3d0d747224e11ff82393aa98cf4e0ebd5bf73e5c18b754cce2123b770e192e50
+size 5329
