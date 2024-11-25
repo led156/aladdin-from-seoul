69b69ddd1da53636dcd21382b24f76167cee5272
led156 <led156@naver.com>
2024-11-25 10:31:36 +0900

    🐛 북마크 1개 조회 api 오타
    Former-commit-id: d099e38d248dd013f543ee7b97097a06a476d3eb

--- FrontEnd/vue-project/src/components/Map/Apt/MapItem.vue
+++ FrontEnd/vue-project/src/components/Map/Apt/MapItem.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:575dcbf7e3198702e64630d4ece5405105fe51bc141a524f3e3d47265ad15483
-size 4648
+oid sha256:4cf67b51cd286548f3aa27c155fa2605b99fc68881c98cd85d835db61def7193
+size 4649
