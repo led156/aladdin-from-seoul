41defd13f83917ec3c7da32df555dc4170ee5ac4
led156 <led156@naver.com>
2024-11-25 16:20:28 +0900

    🐛 트라이 검색 api 주소 수정
    Former-commit-id: 9bde35ae3c16d6257ff1d3ca4c74d6c772472ec7 [formerly 80aa9a2b7a8065865ba4359b66a8947aca810d32]
    Former-commit-id: d5351ada2f23a6641bbaffc2ce5ae4992e64550a

--- FrontEnd/vue-project/src/components/Util/SearchAutocomplete.vue
+++ FrontEnd/vue-project/src/components/Util/SearchAutocomplete.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe8ceec42febaabe28e6ba78d36b3419b6569fdf20c897f862d6bf2f2116abcc
-size 6284
+oid sha256:2c30341e5ed44c94bcc06e49289247c0a3c3d135c4fd4bab4a1cdd80d5de6fb1
+size 6283
