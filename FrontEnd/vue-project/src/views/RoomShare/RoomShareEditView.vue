e75c3753681602cfd9ad15ffc801c16fda260b46
led156 <led156@naver.com>
2024-11-25 10:07:06 +0900

    🐛 roomBoardVo 변경
    Former-commit-id: 4059aca336fb725fffbd51422a1b6292f9c7cbf0

--- FrontEnd/vue-project/src/views/RoomShare/RoomShareEditView.vue
+++ FrontEnd/vue-project/src/views/RoomShare/RoomShareEditView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:304f5d91310e4b28a13acdf4d1f2ab8ad2e00acd8b1f7b4f2f2c7cfa697e2e20
-size 15332
+oid sha256:273cbaeaf8f4800ca044f20079fffc0c5f774bf610b5e9ef58b4ff1a2e9c8094
+size 15317
