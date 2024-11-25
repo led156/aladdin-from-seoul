39108ce07eeff06c164f35e04bfda7f795c99afd
led156 <led156@naver.com>
2024-11-25 09:09:41 +0900

    🎨 roomBoardVo 변수이름 변경
    Former-commit-id: 25bd90ce889d6155b1b1cec085f969d62dd636c6

--- FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ShareContent.vue
+++ FrontEnd/vue-project/src/components/Map/Bookmark/InfoWidgets/ShareContent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d8665d207f9322acb908048595798c42bcca6011a727fb9b24cce3d54a553c5
-size 11351
+oid sha256:db25868e2ea90deae5a6c136fce5a50f805938cf5cbd309ac39f4a21e0918adf
+size 11340
