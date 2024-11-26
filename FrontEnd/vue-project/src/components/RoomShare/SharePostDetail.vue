99e95b30ae9e6e57b18de1be5d0bd942e629c640
led156 <led156@naver.com>
2024-11-26 17:22:46 +0900

    🐛 글 상세조회 시 프로필 사진 보이도록
    Former-commit-id: 079021324008d76b0c4086077e0c96454099bd6e

--- FrontEnd/vue-project/src/components/RoomShare/SharePostDetail.vue
+++ FrontEnd/vue-project/src/components/RoomShare/SharePostDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2f682b235ac6e1c91944fe6e530d5a04e397d988385d767965910cd86f94d7e
-size 16922
+oid sha256:c812cf4504ab16b70aa886da37663ac2ad46fc4430546ee169185ddf7aea73cf
+size 16921
