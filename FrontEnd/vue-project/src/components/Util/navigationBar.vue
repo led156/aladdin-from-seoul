d4776e93b9a348f996f7006e0f43f0499b7b7063
led156 <led156@naver.com>
2024-11-26 19:01:52 +0900

    💄 UI 수정
    - 마이페이지 카드
    - 네비게이션바
    - 프로필수정
    
    
    Former-commit-id: 13f411ead849d06e6e5f3ca7592eb7be0b454a68 [formerly 183cd23d9454c391a40563586ca0a037bcd173a9]
    Former-commit-id: 563b76b819a6728b56f1204d58dd17f98ca6ddde

--- FrontEnd/vue-project/src/components/Util/navigationBar.vue
+++ FrontEnd/vue-project/src/components/Util/navigationBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:903ab949007cdc2fd13e95bab882e29a19bc1e57e6c41dc1c32ff068b8d7a61d
-size 4014
+oid sha256:6b903e6d8cca28579660ef03e6672443d85c100dbee0fd39049835e93531a65f
+size 4227
--- FrontEnd/vue-project/src/views/User/EditProfilePage.vue
+++ FrontEnd/vue-project/src/views/User/EditProfilePage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88f312d43d5204b77003c14a6e2139120d862510d5ee5114e2fd4aff96b1d9c4
-size 15320
+oid sha256:150bea40045c18aba9a9847aedb74018abc7bb14fac92f85b5ab46ea26228bdf
+size 16864
