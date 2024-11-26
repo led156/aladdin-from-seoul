5570647b28c3b56cc5edc0ccdd5b2739adf5025d
배석진 <bsj1044@naver.com>
2024-11-27 03:38:44 +0900

    🐛게시글 북마크 북마크 저장 시각 기준으로 조회되도록 수정
    Former-commit-id: 912193d7f9741e4fac4985498c5a5aa9c8b3b1e3

--- BackEnd/springboot-project/src/main/java/com/aladin/comment/mapper/CommentMapper.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/comment/mapper/CommentMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cd16b07b673581a7678966c01881b9dde302e30f9b3cc75cfa1e2eed8885fbc
-size 728
+oid sha256:aadae8e3c4df8b241742a6348becab67f627c583f3aa8549d4abfcf7415d01b1
+size 731
